815403c1c12ff96d9b875339b2c8faeb6c5eee07
SrijanG05 <gsrijan777@gmail.com>
2026-04-17 10:36:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let timerDisplay=document.getElementById("timer");
+let currentDate=new Date();
+set Interval{
+   function()
+	   {
+		   currentDate=new Date();
+		   timerDisplay.innerHTML=currentDate.tolocaleString();
+},1000
+}
